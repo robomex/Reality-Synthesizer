5e129ec6fd68fde5104f30df3762317ed86505c7
Tony Morales <atmorales@gmail.com>
2022-04-01 23:57:29 -0500

    Initial Commit

---
+++ Reality Synthesizer/Reality_SynthesizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reality_SynthesizerApp.swift
+//  Reality Synthesizer
+//
+//  Created by Tony Morales on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Reality_SynthesizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
